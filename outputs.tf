17ba7936149eb0c14de9b5b7bda74b117444a4a8
jtreguboff <jonathan.treguboff@odeza.com>
2022-01-20 13:26:42 -0700

    Added a output for the ARN.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "ec2_instance_id" {
-  value = aws_instance.sample.id
+  value       = aws_instance.sample.id
   description = "The AWS ID of the Ec2 Instance we created."
+}
+
+output "tags" {
+  value       = aws_instance.sample.arn
+  description = "The ARN of the instance we created."
 }
