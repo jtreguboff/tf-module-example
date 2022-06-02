5e42914edd22c5b735bb87094d5e0d3fca9b3850
jtreguboff <jonathan.treguboff@odeza.com>
2022-06-02 10:53:17 -0700

    subnet

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "sample" {
   ami           = var.ec2_ami_id
   instance_type = var.ec2_instance_type
+  subnet_id     = "subnet-0e9405662a7302ec5"
   tags = {
     Name = var.ec2_instance_name
   }
